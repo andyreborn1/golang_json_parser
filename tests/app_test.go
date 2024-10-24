d69216a44dc7ee7864238232eebab53d3059a127
Anderson Vinicius Ribeiro <38798381+andyreborn1@users.noreply.github.com>
2024-10-23 22:47:58 -0300

    test(valid_test): add invalid json test

--- tests/app_test.go
+++ tests/app_test.go
@@ -27,3 +27,24 @@ func TestJsonParserValido(t *testing.T) {
 		})
 	}
 }
+
+func TestJsonParserInvalido(t *testing.T) {
+	testFiles := []string{
+		"step1/invalid.json",
+	}
+
+	for _, f := range testFiles {
+		strFromJson, _ := os.ReadFile(f)
+		filenameRegex, _ := regexp.Compile("(\\w+).json$")
+
+		teste := filenameRegex.FindAllStringSubmatch(f, -1)
+
+		res := IsJsonValid(string(strFromJson))
+
+		t.Run(teste[0][1], func(t *testing.T) {
+			if false != res {
+				t.Error(`Json Inválido`)
+			}
+		})
+	}
+}
