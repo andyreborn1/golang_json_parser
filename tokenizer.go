c9b6af75d650919a4dfe56500d4a290bb44af409
Anderson Vinicius Ribeiro <38798381+andyreborn1@users.noreply.github.com>
2024-10-23 22:28:17 -0300

    feat(tokenizer): implements the tokenizer

---
+++ tokenizer.go
@@ -0,0 +1,76 @@
+package jsonParser
+
+type Tokenizer struct {
+	Source string
+	cursor int64
+	start  int64
+	line   int64
+	tokens []*Token
+}
+
+func (t *Tokenizer) NewTokenizer(str string) *Tokenizer {
+	t.line = 1
+	return &Tokenizer{Source: str}
+}
+
+func (t *Tokenizer) Scan() []*Token {
+	for !t.isEOF() {
+		t.start = t.cursor
+		token := t.getNextToken()
+
+		if token != nil {
+			t.tokens = append(t.tokens, token)
+		} else {
+			t.tokens = append(t.tokens, nil)
+			break
+		}
+
+	}
+
+	if t.tokens[len(t.tokens)-1] != nil {
+		token := new(Token)
+
+		t.tokens = append(t.tokens, token.NewToken(EOF, "EOF"))
+	}
+
+	return t.tokens
+}
+
+func (t *Tokenizer) hasMoreTokens() bool {
+	return t.cursor < int64(len(t.Source))
+}
+
+func (t *Tokenizer) isEOF() bool {
+	return t.cursor == int64(len(t.Source))
+}
+
+func (t *Tokenizer) getNextToken() *Token {
+	char := *t.advance()
+
+	token := new(Token)
+
+	switch char {
+	case "{":
+		token = token.NewToken(LeftBrace, char)
+	case "}":
+		token = token.NewToken(RightBrace, char)
+	default:
+		token = nil
+	}
+
+	return token
+}
+
+func (t *Tokenizer) advance() *string {
+	var char string
+
+	if t.hasMoreTokens() {
+		char = t.Source[t.cursor : t.cursor+1]
+	} else {
+		return nil
+	}
+
+	t.cursor++
+
+	return &char
+}
