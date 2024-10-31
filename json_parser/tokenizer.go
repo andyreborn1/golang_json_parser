58b89825b624788f3e5e331e4bfe6e43ba19fe66
Anderson Vinicius Ribeiro <38798381+andyreborn1@users.noreply.github.com>
2024-10-31 20:05:37 -0300

    add numbers and keywords to the tokenizer

--- json_parser/tokenizer.go
+++ json_parser/tokenizer.go
@@ -1,5 +1,12 @@
 package json_parser
 
+import (
+	"errors"
+	"log"
+	"regexp"
+	"strconv"
+)
+
 type Tokenizer struct {
 	Source string
 	cursor int64
@@ -54,29 +61,36 @@ func (t *Tokenizer) isEOF() bool {
 }
 
 func (t *Tokenizer) getNextToken() *Token {
-	char := *t.advance()
+	//char := *t.advance()
+	numberRegex := regexp.MustCompile("\\d")
+	stringRegex := regexp.MustCompile("\\w")
+	spaceRegex := regexp.MustCompile("\\s+")
 
 	token := new(Token)
 
-	switch char {
-	case "{":
+	switch char := *t.advance(); {
+	case char == "{":
 		token = token.NewToken(LeftBrace, char)
-	case "}":
+	case char == "}":
 		token = token.NewToken(RightBrace, char)
-	case "[":
+	case char == "[":
 		token = token.NewToken(LeftBracket, char)
-	case "]":
+	case char == "]":
 		token = token.NewToken(RightBracket, char)
-	case ":":
+	case char == ":":
 		token = token.NewToken(Colon, char)
-	case ",":
+	case char == ",":
 		token = token.NewToken(Comma, char)
-	case "\n":
+	case char == "\n":
 		t.line++
-	case " ":
+	case spaceRegex.MatchString(char):
 		token = token.NewToken(Undefined, char)
-	case `"`:
+	case char == `"`:
 		token = t.getStringToken()
+	case numberRegex.MatchString(char):
+		token = t.getNumberToken(numberRegex)
+	case stringRegex.MatchString(char):
+		token = t.getKeywordToken(stringRegex)
 	default:
 		token = nil
 	}
@@ -90,12 +104,61 @@ func (t *Tokenizer) getStringToken() *Token {
 	}
 
 	if t.isEOF() {
+		log.Println(errors.New("unexpected end of file"))
 		return nil
 	}
 
 	t.advance()
 
 	return &Token{TokenType: String, Value: t.Source[t.start+1 : t.cursor-1]}
+
+}
+
+func (t *Tokenizer) getKeywordToken(stringRegex *regexp.Regexp) *Token {
+	for (!t.isEOF()) && (stringRegex.MatchString(t.peek())) {
+		t.advance()
+	}
+
+	switch kw := t.Source[t.start:t.cursor]; {
+	case kw == "true", kw == "false":
+		boolKw, _ := strconv.ParseBool(kw)
+		return &Token{TokenType: Boolean, Value: boolKw}
+	case kw == "null":
+		return &Token{TokenType: Null, Value: nil}
+	default:
+		return nil
+	}
+}
+
+func (t *Tokenizer) getNumberToken(numberRegex *regexp.Regexp) *Token {
+	for (!t.isEOF()) && (numberRegex.MatchString(t.peek())) {
+		t.advance()
+	}
+
+	if t.isEOF() {
+		return nil
+	}
+
+	var intNum JsonValue
+
+	if t.peek() == "." {
+		if !numberRegex.MatchString(t.peekNext()) {
+			log.Println(errors.New("invalid number"))
+			return nil
+		}
+
+		t.advance()
+
+		for numberRegex.MatchString(t.peek()) {
+			t.advance()
+		}
+
+		intNum, _ = strconv.ParseFloat(t.Source[t.start:t.cursor], 32)
+	} else {
+		intNum, _ = strconv.ParseInt(t.Source[t.start:t.cursor], 10, 32)
+	}
+
+	return &Token{TokenType: Number, Value: intNum}
 
 }
 
@@ -119,4 +182,12 @@ func (t *Tokenizer) peek() string {
 	}
 
 	return t.Source[t.cursor : t.cursor+1]
+}
+
+func (t *Tokenizer) peekNext() string {
+	if t.cursor+1 >= int64(len(t.Source)) {
+		return ""
+	}
+
+	return t.Source[t.cursor+1 : t.cursor+2]
 }
